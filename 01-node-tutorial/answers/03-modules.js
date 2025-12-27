e9ea4b9e0cf80bf61f3a81658e01d4f0c0298de5
Dayana <dayileyvas@gmail.com>
2025-12-27 17:47:14 -0600

    some little changes

--- 01-node-tutorial/answers/03-modules.js
+++ 01-node-tutorial/answers/03-modules.js
@@ -1,6 +1,6 @@
 const names = require("./04-names.js");
 const sayHi = require("./05-utils.js");
-const flavor = require("./06-alternative-flavors.js");
+const flavor = require("./06-alternative-flavor.js");
 require("./07-mind-grenade.js");
 
 console.log(names);
